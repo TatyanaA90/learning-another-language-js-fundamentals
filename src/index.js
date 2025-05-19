28691a09ac37d1e5d32457132de4408af290ef31
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-05-19 11:47:39 -0700

    Co-authored-by: Sno <ra-choa@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 // ======================
 //      VARIABLES
 // ======================
-// veggie = "carrot"
-// veggie = "onion"
+// veggie = 'carrot'
+// veggie = 'onion'
+//
 
+let veggie = 'carrot';
+veggie = 'onion';
+console.log(veggie);
+
+// console.log('hey');
 //
 //
 // =========================
 //      DATA STRUCTURES
 // =========================
 
-// animals = ["tiger", "panda", "raccoon"]
-// animals.append("opossum")
+// animals = ['tiger', 'panda', 'raccoon']
+// animals.append('opossum')
+let animals = ['tiger', 'panda', 'raccoon'];
+animals.push('opossum');
 
-// dog = {"name": "Sprinkles", "age": 3}
-// dog["is_friendly"] = True
+// dog = {'name': 'Sprinkles', 'age': 3}
+// dog['is_friendly'] = True
+let dog = { name: 'Sprinkles', age: 3 };
+dog['is.friendly'] = true;
+console.log(dog);
 
 //
 //
@@ -25,13 +36,23 @@
 // num = 5
 // for i in range(num):
 //    print(i)
+let num = 5;
+for (let i = 0; i < num; i++) {
+  console.log(i);
+}
 
 // for animal in animals:
 //    print(animal)
+for (let animal of animals) {
+  console.log(animal);
+}
 
 // for k, v in dog.items():
-//    print(f"{k}: {v}")
-
+//    print(f'{k}: {v}')
+console.log('test');
+for (const [k, v] of Object.entries(dog)) {
+  console.log(`${k} ${v}`);
+}
 //
 //
 // ===================================
@@ -40,19 +61,30 @@
 
 // def fizzbuzz(num):
 //    if num % 3 == 0 and num % 5 == 0:
-//        return "fizzbuzz"
+//        return 'fizzbuzz'
 //    elif num % 3 == 0:
-//        return "fizz"
+//        return 'fizz'
 //    elif num % 5 == 0:
-//        return "buzz"
+//        return 'buzz'
 //    else:
 //        return num
 
+const fizzbuzz = function (num) {
+  if (num % 3 == 0 && num % 5 == 0) {
+    return 'fizzbuzz';
+  } else if (num % 3 == 0) {
+    return 'fizz';
+  } else if (num % 5 == 0) {
+    return 'buzz';
+  } else {
+    return num;
+  }
+};
+console.log(fizzbuzz(12));
+
 // Old way: Function Declaration
 
-//
 // ES6 way: Function Expression
-
 //
 //
 // ======================
@@ -65,12 +97,43 @@
 //        self.name = name
 //
 //    def __str__(self):
-//        return f"{self.name} is a {self.species}"
+//        return f'{self.name} is a {self.species}'
+class Animal1 {
+  constructor(species, name) {
+    this.species = species;
+    this.name = name;
+  }
+  toString() {
+    return `${this.name} is a ${this.species}`;
+  }
+}
+const newanimal = new Animal1('dog', 'Rex');
+console.log(newanimal.toString());
 
 // class Dog(Animal):
 //    def __init__(self, name, trick):
-//        super().__init__(species="dog", name=name)
+//        super().__init__(species='dog', name=name)
 //        self.trick = trick
 //
 //    def __str__(self):
-//        return f"{super().__str__()} that can {self.trick}"
+//        return f'{super().__str__()} that can {self.trick}';
+class Animal {
+  constructor(species, name) {
+    this.species = species;
+    this.name = name;
+  }
+  toString() {
+    return `${this.name} is a ${this.species}`;
+  }
+}
+class Dog extends Animal {
+  constructor(name, trick) {
+    super('dog', name);
+    this.trick = trick;
+  }
+  toString() {
+    return `${super.toString()} that can ${this.trick}`;
+  }
+}
+const mydog = new Dog('Lola', 'sleep');
+console.log(mydog.toString());
